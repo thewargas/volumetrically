dd0ec2dedcb82c84b8a3c4e612ff6d5e4cf9e5fc
Vasiliy Lavrov <71934240+thewargas@users.noreply.github.com>
2023-06-17 15:47:56 +0300

    Update cors.js

--- Backend/middlewares/cors.js
+++ Backend/middlewares/cors.js
@@ -10,7 +10,7 @@ const allowedCors = [
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS';
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
