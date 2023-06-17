a8f1c25340d510031e084d4a2ff49184e99ecfd5
Vasiliy Lavrov <71934240+thewargas@users.noreply.github.com>
2023-06-17 18:02:07 +0300

    Update cors.js

--- Backend/middlewares/cors.js
+++ Backend/middlewares/cors.js
@@ -27,7 +27,7 @@
 //   return next();
 // };
 
-module.exports = (req, res, next) {
+module.exports = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
