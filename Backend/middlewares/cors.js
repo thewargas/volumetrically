b9da83281ce7b600bd69cf4fd49c45a4291ff8ac
Vasiliy Lavrov <71934240+thewargas@users.noreply.github.com>
2023-06-17 18:10:45 +0300

    Update cors.js

--- Backend/middlewares/cors.js
+++ Backend/middlewares/cors.js
@@ -1,36 +1,36 @@
-// const allowedCors = [
-//   'https://thewargas.nomoredomains.monster',
-//   'http://thewargas.nomoredomains.monster',
-//   'https://api.thewargas.nomoredomains.monster',
-//   'http://api.thewargas.nomoredomains.monster',
-//   'http://localhost:3000',
-//   'https://localhost:3000',
-// ];
-
-// module.exports = (req, res, next) => {
-//   const { origin } = req.headers;
-//   const { method } = req;
-//   console.log(origin);
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
-//   const requestHeaders = req.headers['access-control-request-headers'];
-
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//   }
-
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
-
-//   return next();
-// };
+const allowedCors = [
+  'https://thewargas.nomoredomains.monster',
+  'http://thewargas.nomoredomains.monster',
+  'https://api.thewargas.nomoredomains.monster',
+  'http://api.thewargas.nomoredomains.monster',
+  'http://localhost:3000',
+  'https://localhost:3000',
+];
 
 module.exports = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-     return next();
-}
+  const { origin } = req.headers;
+  const { method } = req;
+  console.log(origin);
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+};
+
+// module.exports = (req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//      return next();
+// }
 
