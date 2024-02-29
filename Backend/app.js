1b11a180bf72e2749f359be59a6293266d6bda91
Vasiliy <thewargas@mail.ru>
2024-02-29 23:02:34 +0300

    Fix: cors

--- Backend/app.js
+++ Backend/app.js
@@ -1,23 +1,21 @@
-const express = require('express');
-const multer = require('multer');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
-const router = require('./routes');
-const selectErrors = require('./middlewares/errors');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const enableCors = require('./middlewares/cors');
+const express = require("express");
+const multer = require("multer");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const { errors } = require("celebrate");
+const router = require("./routes");
+const selectErrors = require("./middlewares/errors");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const enableCors = require("./middlewares/cors");
 
-const {
-  PORT = 3003,
-  DB_URL = 'mongodb://127.0.0.1:27017/modelsdb',
-} = process.env;
+const { PORT = 3003, DB_URL = "mongodb://127.0.0.1:27017/modelsdb" } =
+  process.env;
 const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
-    cb(null, 'uploads');
+    cb(null, "uploads");
   },
   filename: (_, file, cb) => {
     cb(null, file.originalname);
@@ -30,14 +28,16 @@ mongoose.connect(DB_URL);
 
 app.use(express.json());
 
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 app.use(enableCors);
 
-app.post('/upload', upload.single('file'), (req, res) => {
+app.use("/upload", cors());
+
+app.post("/upload", upload.single("file"), (req, res) => {
   res.json({
     url: `/uploads/${req.file.originalname}`,
   });
